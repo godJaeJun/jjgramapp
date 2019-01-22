227a094d01a116af1768b67a1e0f08937201c67c
이재준 <asapz@naver.com>
2019-01-22 10:36:16 +0900

    Preloading Assets

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading, Asset, Font } from "expo";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
 
-export default class App extends React.Component {
+class App extends React.Component {
+  state = {
+    isLoadingComplete: false  //로딩
+  };
   render() {
+    const { isLoadingComplete } = this.state;
+    if (!isLoadingComplete) {//로딩이 false면
+      return (
+        //AppLoading은 3개의 옵션필요 불러올것, 에러, 불러오고 나서의 액션
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
+  _loadAssetsAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require("./assets/images/logo.png"),
+        require("./assets/images/logo-white.png"),
+        require("./assets/images/noPhoto.jpg"),
+        require("./assets/images/photoPlaceholder.png")
+      ]),
+      Font.loadAsync({
+        ...Ionicons.font,
+        ...MaterialIcons.font
+      })
+    ]);
+  };
+  _handleLoadingError = error => {
+    console.error(error);
+  };
+  _handleFinishLoading = async () => {
+    this.setState({
+      isLoadingComplete: true
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+export default App;
