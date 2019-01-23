0ad2be6d2fdbcd5723b3b378ead92a9e855111ef
이재준 <asapz@naver.com>
2019-01-23 15:48:38 +0900

    LoginScreen

--- screens/LogInScreen/container.js
+++ screens/LogInScreen/container.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
+import { Alert } from "react-native";
 import LogInScreen from "./presenter";
 
 class Container extends Component{
+    state={
+        username:"",
+        password:"",
+        isSubmitting:false
+    };
     //네비게이션 옵션을 설정한다. title 상단이름 정하기, header=null은 상단 없애기
     static navigationOptions=({navigation}) => ({
-        title:'LogIn'
+        header:null
     });
     render(){
-        return <LogInScreen/>;
+        return <LogInScreen {...this.state} 
+        changeUsername={this._changeUsername}
+        changePassword={this._changePassword}
+        submit={this._submit}
+        />;
+    }
+    _changeUsername=(text)=>{
+        this.setState({username:text})
+    }
+    _changePassword=(text)=>{
+        this.setState({password:text})
+    }
+    _submit =()=>{
+        const {username,password,isSubmitting} = this.state;
+        if(!isSubmitting){
+            if(username&&password){
+                this.setState({
+                    isSubmitting:true
+                });
+                //redux action
+            }
+            else{
+                Alert.alert("All fields are required");
+            }
+        }
     }
 }
 
