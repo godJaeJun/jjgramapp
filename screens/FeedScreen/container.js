86775cb47399e2d03add3bb72d057fd2faa346f6
이재준 <asapz@naver.com>
2019-01-24 20:41:49 +0900

    Customizing the Profile Header

---
+++ screens/FeedScreen/container.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import FeedScreen from "./presenter";
+import { Image } from "react-native";
+import NavButton from "../../components/NavButton";
+
+class Container extends Component{
+    render(){
+        return <FeedScreen {...this.props}/>
+    }
+}
+export default Container;
